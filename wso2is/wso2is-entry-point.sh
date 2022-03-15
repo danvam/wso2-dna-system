a22a887a069d65ad1f062bb4619daa4f79b8459e
María José Sánchez <mariajose.sanchez@aeptic.es>
2022-03-15 23:44:28 +0100

    certificados y comandos

--- wso2is/wso2is-entry-point.sh
+++ wso2is/wso2is-entry-point.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #wso2is-entrypoint.sh
 cp -R /home/wso2carbon/shared/lib/* /home/wso2carbon/wso2is-5.11.0/repository/components/lib
-cp /home/wso2carbon/shared/conf/* /home/wso2carbon/wso2is-5.11.0/repository/conf
-# cp /home/wso2carbon/shared/security/* /home/wso2carbon/wso2is-5.11.0/repository/resources/security/
+cp -R /home/wso2carbon/shared/conf/* /home/wso2carbon/wso2is-5.11.0/repository/conf
+cp -R /home/wso2carbon/shared/security/* /home/wso2carbon/wso2is-5.11.0/repository/resources/security
 # 5.11.0
 /home/wso2carbon/docker-entrypoint.sh
